c0a8b2a63ffd02751c6cc82868cc1e99610caf6c
Gustavo Henrique <133015499+Gustavohb5@users.noreply.github.com>
2023-09-10 20:57:43 -0300

    Design minimalista e página "projeto"
    foi mudado algumas coisas do design para deixar um pouco mais minimalista e adicionado a página projeto

--- index.js
+++ index.js
@@ -1,20 +1,16 @@
 function menuResponsivo(){
     const close = document.getElementById('close')
     const menu = document.getElementById('menu')
-    
     const menuMobile = document.getElementById('menuMobile')
+    
     if (menuMobile.style.display == 'block'){
         menuMobile.style.display = 'none'
         close.style.display = 'none'
         menu.style.display = 'block'
-
     }
     else{
         menuMobile.style.display = 'block'
         close.style.display = 'block'
-        menu.style.display = 'none'
-
-
-        
+        menu.style.display = 'none'    
     }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,9 @@ module.exports = {
         // => @media (min-width: 640px) { ... }
         'phone-2': '785px',
         // => @media (min-width: 720px) { ... }
-  
+        'nav': '917px',
+        // => @media (min-width: 917px) { ... }
+
         'laptop': '1024px',
         // => @media (min-width: 1024px) { ... }
   
