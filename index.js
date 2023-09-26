cbf18dc46dfb034fa2d2a2670978266d1d84f790
Gustavo Henrique <133015499+Gustavohb5@users.noreply.github.com>
2023-09-26 19:42:47 -0300

    Adicionado a foto dos desenvolvedores
    foto dos desenvolvedores e mais alguns ajustes

--- index.js
+++ index.js
@@ -1,16 +1,47 @@
+//CONFIGURAÇÕES DO TAILWIND
+tailwind.config = {
+    darkMode: 'class',
+    theme: {
+        screens: {
+            'phone': '640px',           
+            'phone-2': '785px',
+            'nav': '1070px',
+            'computer': '1280px',
+        },
+        extend: {
+            colors: {
+            }
+        }
+    }
+}
 function menuResponsivo(){
     const close = document.getElementById('close')
     const menu = document.getElementById('menu')
     const menuMobile = document.getElementById('menuMobile')
-    
-    if (menuMobile.style.display == 'block'){
-        menuMobile.style.display = 'none'
+
+    if(menuMobile.style.left == '0vh'){
+        menuMobile.style.left ='-150vh'
         close.style.display = 'none'
         menu.style.display = 'block'
+
     }
-    else{
-        menuMobile.style.display = 'block'
-        close.style.display = 'block'
-        menu.style.display = 'none'    
-    }
+   else{
+    menuMobile.style.left = '0vh'
+    close.style.display = 'block'
+    menu.style.display = 'none'
+    
+   }
+    
+    
+    // if (menuMobile.style.display == 'block'){
+    //     menuMobile.style.display = 'none'
+    //     close.style.display = 'none'
+    //     menu.style.display = 'block'
+    // }
+    // else{
+    //     menuMobile.style.display = 'block'
+    //     close.style.display = 'block'
+    //     menu.style.display = 'none'    
+    // }
 }
+
